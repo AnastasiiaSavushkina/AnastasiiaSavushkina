37da392c73d7e8d9b84b437fde810bc054c62eaf
AnastasiiaSavushkina <anaasico@gmail.com>
2018-07-22 20:47:55 +0300

    load2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -68,10 +68,10 @@
         if (!isMobile) {
             setTimeout(function() {
                 preloader.addClass('preloaded');
-            }, 50);
+            }, 800);
             setTimeout(function() {
                 preloader.remove();
-            }, 50);
+            }, 2000);
 
         } else {
             preloader.remove();
